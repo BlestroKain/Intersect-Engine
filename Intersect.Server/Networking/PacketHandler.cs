31b281421dd3524ca3c6033c1cd131ef7c81158c
Bryan <b.martinez@kation.com.do>
2023-07-12 23:39:09 -0400

    feat: :sparkles: Add logic for death system

--- Intersect.Server/Networking/PacketHandler.cs
+++ Intersect.Server/Networking/PacketHandler.cs
@@ -2904,6 +2904,19 @@ namespace Intersect.Server.Networking
             }
             player.PictureClosed(packet.EventId);
         }
+        
+        //RespawnPacket
+        public void HandlePacket(Client client, RespawnPacket packet)
+        {
+            var player = client?.Entity;
+            if (player == null)
+            {
+                return;
+            }
+
+            player?.Reset();
+            player?.Respawn();
+        }
 
         #endregion
 
